5bc39e6d1aa9ec03e6eb3b9f20c403eddb06e629
Austin File <austinfile510@gmail.com>
2021-01-25 12:11:12 -0800

    initial commit

---
+++ src/recipes/recipes-service.js
@@ -0,0 +1,28 @@
+const xss = require ('xss')
+
+const RecipesService = {
+    getAllRecipes(db) {
+
+    },
+
+    getRecipeById(db, id) {
+
+    },
+
+    getRecipeByMealType(db, meal_type) {
+
+    },
+
+    getRecipeByUser(db, user_id) {
+        
+    }
+
+    editRecipe(db, id) {
+
+    },
+
+    deleteRecipe(db, id) {
+
+    },
+
+}
