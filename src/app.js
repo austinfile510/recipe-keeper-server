1929052b3c498ae5dafd8d548fc91f8b8d0ff6f0
Austin File <austinfile510@gmail.com>
2021-02-09 10:41:35 -0800

    updated app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, API_ENDPOINT } = require('./config');
 const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
 const recipesRouter = require('./recipes/recipes-router');
@@ -14,12 +14,18 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(
+	cors({
+		origin: API_ENDPOINT,
+	})
+);
 
-app.use('/api/auth', authRouter);
+app.use('/api/recipes', recipesRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/recipes', recipesRouter)
-app.use('/api/my-recipes', myRecipesRouter)
+app.use('/api/my-recipes', myRecipesRouter);
+app.use('/api/auth', authRouter);
+
+
 app.get('/', (req, res) => {
 	res.send('Hello, world!');
 });
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://localhost:3000/api",
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://localhost:3000",
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:interactive@localhost/recipe-keeper',
     TEST_DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:interactive@localhost/recipe-keeper-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
