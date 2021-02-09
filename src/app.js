ce607382407b5c53c3e8677341b06509f2c02018
Austin File <austinfile510@gmail.com>
2021-02-08 22:32:38 -0800

    updated app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors( CLIENT_ORIGIN ));
+app.use(cors({ origin: CLIENT_ORIGIN }));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
