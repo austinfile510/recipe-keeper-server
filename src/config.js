edd107eb969d4d773cbf43022c26373627f777a0
Austin File <austinfile510@gmail.com>
2021-02-09 08:06:23 -0800

    reconfigured app.js and config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,6 @@ module.exports = {
     API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://localhost:3000/api",
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:interactive@localhost/recipe-keeper',
     TEST_DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:interactive@localhost/recipe-keeper-test',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://recipe-keeper-client.vercel.app',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '1d',
 
