8f496df11d13116dd781e7744a9b22aaf3b7502a
Austin File <austinfile510@gmail.com>
2021-02-09 06:31:42 -0800

    enabled cors variable in app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
 const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
 const recipesRouter = require('./recipes/recipes-router');
@@ -14,7 +14,7 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: CLIENT_ORIGIN }));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
